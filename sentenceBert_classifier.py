7612559dec7e532a9a788611bf4d0224c7635492
heyix <heyixu0@gmail.com>
2023-04-12 02:28:51 -0400

    Update sentenceBert_classifier.py

--- sentenceBert_classifier.py
+++ sentenceBert_classifier.py
@@ -65,43 +65,16 @@ mse_list=[]
 #     mse_list.append(mean_squared_error(y_test, y_test_pred))
 
 #SVM
-for fold, (train_index, test_index) in enumerate(kf.split(X_encoded)):
-    print(f'Fold {fold + 1}')
-    X_train, X_test = X_encoded[train_index], X_encoded[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    model = SVR(kernel='linear')
-    model.fit(X_train, y_train)
-
-    y_train_pred = model.predict(X_train)
-    y_test_pred = model.predict(X_test)
-    y_test_pred=np.clip(y_test_pred, a_min=None, a_max=5.0)
-    print(f'Test MSE at iterartion {fold+1} : {mean_squared_error(y_test, y_test_pred)}')
-
-
-    mse_list.append(mean_squared_error(y_test, y_test_pred))
-
-
-#RandomForest
 # for fold, (train_index, test_index) in enumerate(kf.split(X_encoded)):
 #     print(f'Fold {fold + 1}')
 #     X_train, X_test = X_encoded[train_index], X_encoded[test_index]
 #     y_train, y_test = y[train_index], y[test_index]
 #
+#     scaler = StandardScaler()
+#     X_train = scaler.fit_transform(X_train)
+#     X_test = scaler.transform(X_test)
 #
-#     forest_parameters={
-#         'n_estimators':300,
-#         'max_depth':15,
-#         'min_samples_split':2,
-#         'min_samples_leaf':1,
-#         'random_state':42
-#     }
-#     # Perform linear regression and evaluate the model using the training and testing sets
-#     model = RandomForestRegressor(**forest_parameters)
+#     model = SVR(kernel='linear')
 #     model.fit(X_train, y_train)
 #
 #     y_train_pred = model.predict(X_train)
@@ -111,6 +84,33 @@ for fold, (train_index, test_index) in enumerate(kf.split(X_encoded)):
 #
 #
 #     mse_list.append(mean_squared_error(y_test, y_test_pred))
+
+
+#RandomForest
+for fold, (train_index, test_index) in enumerate(kf.split(X_encoded)):
+    print(f'Fold {fold + 1}')
+    X_train, X_test = X_encoded[train_index], X_encoded[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+
+    forest_parameters={
+        'n_estimators':300,
+        'max_depth':15,
+        'min_samples_split':2,
+        'min_samples_leaf':1,
+        'random_state':42
+    }
+    # Perform linear regression and evaluate the model using the training and testing sets
+    model = RandomForestRegressor(**forest_parameters)
+    model.fit(X_train, y_train)
+
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+    y_test_pred=np.clip(y_test_pred, a_min=None, a_max=5.0)
+    print(f'Test MSE at iterartion {fold+1} : {mean_squared_error(y_test, y_test_pred)}')
+
+
+    mse_list.append(mean_squared_error(y_test, y_test_pred))
 
 mean_mse = np.mean(mse_list)
 print(f"Mean MSE across all train/test splits: {mean_mse:.4f}")
